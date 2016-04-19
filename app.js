772bc2f88e0bb07b5adb041e826136b1a9144a04
Dominic Breuker <dominic.breuker@gmail.com>
2016-04-19 11:15:59 +0200

    try second example

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-var restify = require('restify');
 var builder = require('botbuilder');
 
-// Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
-bot.add('/', function (session) {
-    session.send('Hello World');
+var helloBot = new builder.TextBot();
+helloBot.add('/', function (session) {
+    if (!session.userData.name) {
+        session.beginDialog('/profile');
+    } else {
+        session.send('Hello %s!', session.userData.name);
+    }
 });
+helloBot.add('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
 
-// Setup Restify Server
-var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
-});
+helloBot.listenStdin();
